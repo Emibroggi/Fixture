f9680487312ebc09b02d824ed3b056dbf66d3e65
Emiliano Broggi <broggi4@hotmail.com>
2022-11-23 19:50:11 -0300

    componentes

--- Fixture/src/app/app.module.ts
+++ Fixture/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { TablesComponent } from './components/tables/tables.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    TablesComponent
   ],
   imports: [
     BrowserModule,
